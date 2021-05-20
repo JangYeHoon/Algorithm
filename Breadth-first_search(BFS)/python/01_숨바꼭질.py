b61a9ab946690406d22a27a207a5578d62f8e6b7
JangYeHoon <jkjk128jkjk@gmail.com>
2021-05-20 22:52:52 +0900

    bfs 숨바꼭질

---
+++ Breadth-first_search(BFS)/python/01_숨바꼭질.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+n, k = map(int, input().split())
+visited = [0] * 100001
+
+def bfs():
+    q = deque([n])
+    while q:
+        x = q.popleft()
+        if x == k:
+            return visited[x]
+        for next in (x - 1, x + 1, x * 2):
+            if 0 <= next < 100001 and visited[next] == 0:
+                visited[next] = visited[x] + 1
+                q.append(next)
+
+print(bfs())
