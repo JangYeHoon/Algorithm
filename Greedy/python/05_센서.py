8ab4fd219e53ac7c037cae5a4e7cc11eca1c4921
JangYeHoon <jkjk128jkjk@gmail.com>
2021-06-09 21:48:43 +0900

    greedy 문제 풀이

---
+++ Greedy/python/05_센서.py
@@ -0,0 +1,19 @@
+import sys
+
+n = int(input())
+k = int(input())
+if k >= n:
+    print(0)
+    sys.exit()
+
+arr = list(map(int, input().split()))
+arr.sort()
+
+distance = [0] * (n - 1)
+for i in range(n - 1):
+    distance[i] = arr[i + 1] - arr[i]
+
+for i in range(k - 1):
+    distance.remove(max(distance))
+
+print(sum(distance))
--- README.md
+++ README.md
@@ -211,6 +211,7 @@
         <li><a href="Greedy/python/02_뒤집기.py">02. 뒤집기</a></li>
         <li><a href="Greedy/python/03_등수매기기.py">03. 등수매기기</a></li>
         <li><a href="Greedy/python/04_배.py">04. 배</a></li>
+        <li><a href="Greedy/python/05_센서.py">05. 센서</a></li>
     </ul>
 </details>
 
