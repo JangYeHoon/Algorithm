61f1d5148317c7d6149c0b695bab56d816ca68e4
JangYeHoon <jkjk128jkjk@gmail.com>
2022-01-30 07:30:21 +0900

    Jan 4th review-2

--- test.py
+++ test.py
@@ -11,12 +11,14 @@ mx, my = [-1, 1, 0, 0], [0, 0, -1, 1]
 def dfs(x, y, i):
     if matrix[x][y] == 'S':
         return True
-    elif matrix[x][y] == 'O':
+    if matrix[x][y] == 'O':
         return False
     nx = x + mx[i]
     ny = y + my[i]
     if nx >= 0 and nx < N and ny >= 0 and ny < N:
         return dfs(nx, ny, i)
+    else:
+        return False
 
 def check():
     for x, y in teacher:
@@ -29,11 +31,10 @@ for i in range(N):
     temp_list = list(input().split())
     for j in range(N):
         matrix[i][j] = temp_list[j]
-        if matrix[i][j] == 'T':
-            teacher.append((i, j))
-        elif matrix[i][j] == 'X':
+        if matrix[i][j] == 'X':
             space.append((i, j))
-
+        elif matrix[i][j] == 'T':
+            teacher.append((i, j))
 chk = False
 for s in combinations(space, 3):
     for x, y in s:
