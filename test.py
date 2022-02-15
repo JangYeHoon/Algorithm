3c731d84296ffcd3aee43e1f660abfe745984e65
JangYeHoon <jkjk128jkjk@gmail.com>
2022-02-15 09:11:59 +0900

    Update 늑대와양 c++

--- test.py
+++ test.py
@@ -1,25 +1,29 @@
-# 꽃길
+# 꽃길, 늑대와양
 
-S = input()
-result, tmp = "", ""
-tag_chk = False
+R, C = map(int, input().split())
+matrix = [list(input()) for _ in range(R)]
+mx, my = [-1, 1, 0, 0], [0, 0, -1, 1]
 
-for s in S:
-    if s == ' ':
-        if tag_chk:
-            result += s
-        else:
-            result += tmp[::-1] + ' '
-            tmp = ""
-    elif s == '>':
-        tag_chk = False
-        result += '>'
-    elif s == '<':
-        tag_chk = True
-        result += tmp[::-1] + '<'
-        tmp = ""
-    else:
-        if tag_chk: result += s
-        else: tmp += s
-result += tmp[::-1]
-print(result)
+result_chk = True
+for i in range(R):
+    for j in range(C):
+        if matrix[i][j] == 'W':
+            for k in range(4):
+                nx = i + mx[k]
+                ny = j + my[k]
+
+                if nx < 0 or nx >= R or ny < 0 or ny >= C:
+                    continue
+                if matrix[nx][ny] == 'S':
+                    result_chk = False
+
+if result_chk:
+    print(1)
+    for i in range(R):
+        for j in range(C):
+            if matrix[i][j] not in 'SW':
+                matrix[i][j] = 'D'
+    for m in matrix:
+        print(''.join(m))
+else:
+    print(0)
