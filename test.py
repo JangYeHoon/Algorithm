1cb643b4a3bdd80fed2703ac87ae953d9b6d5cb0
JangYeHoon <jkjk128jkjk@gmail.com>
2022-02-26 11:57:42 +0900

    Feb 4th review

--- test.py
+++ test.py
@@ -1,29 +1,41 @@
 # 자물쇠와열쇠, 뱀
 
-def solution(s):
-    answer = 1e9
-    if len(s) == 1:
-        return 1
-    for cnt in range(1, int(len(s) / 2 + 1)):
-        string_cnt = 1
-        tmp = s[0 : 0 + cnt]
-        result = ""
-        for i in range(cnt, len(s), cnt):
-            if tmp == s[i : i + cnt]:
-                string_cnt += 1
-            else:
-                if string_cnt == 1:
-                    result += tmp
-                else:
-                    result += str(string_cnt) + tmp
-                tmp = s[i : i + cnt]
-                string_cnt = 1
-        if string_cnt == 1:
-            result += tmp
-        else:
-            result += str(string_cnt) + tmp
-        answer = min(answer, len(result))
+def rotate90(key):
+    m = len(key)
+    new_key = [[0] * m for _ in range(m)]
+    for i in range(m):
+        for j in range(m):
+            new_key[j][m - i - 1] = key[i][j]
+    return new_key
 
-    return answer
+def lock_chk(lock):
+    n = len(lock) // 3
+    for i in range(n, n * 2):
+        for j in range(n, n * 2):
+            if lock[i][j] != 1:
+                return False
+    return True
 
-print(solution(input()))
+def solution(key, lock):
+    n = len(lock)
+    m = len(key)
+    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
+    for i in range(n):
+        for j in range(n):
+            new_lock[i + n][j + n] = lock[i][j]
+    
+    for x in range(n * 2):
+        for y in range(n * 2):
+            for _ in range(4):
+                key = rotate90(key)
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] += key[i][j]
+                if lock_chk(new_lock):
+                    return True
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] -= key[i][j]
+    return False
+
+solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]])
