489a5496f48104cd695fb2d4f837465a0d3e3a9d
JangYeHoon <jkjk128jkjk@gmail.com>
2022-03-02 09:07:56 +0900

    Add 외벽전검 python

---
+++ Implementation/python/21_외벽전검.py
@@ -0,0 +1,10 @@
+# 
+# https://programmers.co.kr/learn/courses/30/lessons/60062
+# 
+
+def solution(n, weak, dist):
+    answer = 0
+    for i in range(dist):
+        for j in range(dist):
+            
+    return answer
