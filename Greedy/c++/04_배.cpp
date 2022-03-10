4de3a51f725e5b1693304ce22a814f59583d6dc7
JangYeHoon <jkjk128jkjk@gmail.com>
2022-03-10 09:23:19 +0900

    Add 배 c++

---
+++ Greedy/c++/04_배.cpp
@@ -0,0 +1,61 @@
+// fast campus 강의
+// https://www.acmicpc.net/problem/1092
+// 1
+
+// fast campus 강의
+// https://www.acmicpc.net/problem/1092
+// 0
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+vector<int> box;
+vector<int> crane;
+
+bool comp(int a, int b) {
+	return a > b;
+}
+
+int main()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int tmp;
+        cin >> tmp;
+        crane.push_back(tmp);
+    }
+
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        int tmp;
+        cin >> tmp;
+        box.push_back(tmp);
+    }
+    sort(box.begin(), box.end(), comp);
+	sort(crane.begin(), crane.end(), comp);
+        cout << -1;
+
+    int result = 0;
+    if (crane[0] < box[0])
+        result = -1;
+    else {
+        while (1) {
+            if (box.size() == 0)
+                break;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < box.size(); j++) {
+                    if (crane[i] >= box[j]) {
+                        box.erase(box.begin() + j);
+                        break;
+                    }
+                }
+            }
+            result += 1;
+        }
+    }
+    cout << result;
+}
