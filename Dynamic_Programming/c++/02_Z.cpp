7755545a8f966d2f875d2f2a058c74d67b165aca
JangYeHoon <jkjk128jkjk@gmail.com>
2022-06-07 09:04:04 +0900

    Add Z c++

---
+++ Dynamic_Programming/c++/02_Z.cpp
@@ -0,0 +1,46 @@
+// fast campus 강의
+// https://www.acmicpc.net/problem/1074
+// 0
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int result;
+int N, X, Y;
+
+void solve(int n, int x, int y) {
+    if (n == 2) {
+        if (x == X && y == Y) {
+            cout << result;
+            return;
+        }
+        result++;
+        if (x == X && y + 1 == Y) {
+            cout << result;
+            return;
+        }
+        result++;
+        if (x + 1 == X && y == Y) {
+            cout << result;
+            return;
+        }
+        result++;
+        if (x + 1 == X && y + 1 == Y) {
+            cout << result;
+            return;
+        }
+        result++;
+        return;
+    }
+    solve(n / 2, x, y);
+    solve(n / 2, x, y + n / 2);
+    solve(n / 2, x + n / 2, y);
+    solve(n / 2, x + n / 2, y + n / 2);
+}
+
+int main() {
+    cin >> N >> X >> Y;
+    solve(pow(2, N), 0, 0);
+}
