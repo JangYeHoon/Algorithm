2a5cf8fe57f5c14f5090c654b18e644f2090df53
JangYeHoon <jkjk128jkjk@gmail.com>
2022-06-13 20:35:46 +0900

    Add 01타일 c++

---
+++ Dynamic_Programming/c++/04_01타일.cpp
@@ -0,0 +1,21 @@
+// fast campus 강의
+// https://www.acmicpc.net/problem/1904
+// 1
+
+#include <iostream>
+
+using namespace std;
+
+int N;
+long long d[1000001];
+
+int main() {
+	cin >> N;
+	
+	d[1] = 1;
+	d[2] = 2;
+	for (int i = 3; i <= N; i++)
+		d[i] = (d[i - 1] + d[i - 2]) % 15746;
+
+	cout << d[N];
+}
--- Dynamic_Programming/python/04_01타일.py
+++ Dynamic_Programming/python/04_01타일.py
@@ -1,3 +1,7 @@
+# fast campus 강의
+# https://www.acmicpc.net/problem/1904
+# 1
+
 n = int(input())
 
 dp = [0] * 1000001
