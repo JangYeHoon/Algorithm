8a291260ce702b9941e108b8aad7ab1f4f51f1f0
JangYeHoon <jkjk128jkjk@gmail.com>
2022-06-16 09:22:45 +0900

    Add 가장 긴 증가하는 부분 수열 c++

---
+++ Dynamic_Programming/c++/06_가장긴증가하는부분수열.cpp
@@ -0,0 +1,34 @@
+// fast campus 강의
+// https://www.acmicpc.net/problem/11053
+// 0
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int N;
+vector<int> arr;
+int dp[1001];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+
+    int result = 0;
+    for (int i = 0; i < N; i++ ) {
+        dp[i] = 1;
+        for (int j = 0; j < i; j++) {
+            if (arr[j] < arr[i])
+                dp[i] = max(dp[i], dp[j] + 1);
+        }
+        result = max(result, dp[i]);
+    }
+
+    cout << result;
+}
