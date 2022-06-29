51e93927f314d063a862e01f49612a663620ac07
JangYeHoon <jkjk128jkjk@gmail.com>
2022-06-29 09:19:18 +0900

    Update 가장 높은 탑 쌓기 c++

--- Dynamic_Programming/c++/09_가장높은탑쌓기.cpp
+++ Dynamic_Programming/c++/09_가장높은탑쌓기.cpp
@@ -1,6 +1,6 @@
 // fast campus 강의
 // https://www.acmicpc.net/problem/2655
-// 0
+// 1
 
 #include <iostream>
 #include <vector>
--- Dynamic_Programming/python/09_가장높은탑쌓기.py
+++ Dynamic_Programming/python/09_가장높은탑쌓기.py
@@ -1,3 +1,7 @@
+# fast campus 강의
+# https://www.acmicpc.net/problem/2655
+# 1
+
 n = int(input())
 array = []
 array.append((0, 0, 0, 0))
