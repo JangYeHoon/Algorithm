3be389dc4a11752c11c74cc440682db9990bf0f0
JangYeHoon <jkjk128jkjk@gmail.com>
2022-08-25 21:58:34 +0900

    Add 고정점 찾기 python

---
+++ Search/python/18_고정점찾기.py
@@ -0,0 +1,22 @@
+# 이것이 취업을 위한 코딩테스트다
+# 0
+
+def binary_search(arr, start, end):
+    if start > end:
+        return -1
+
+    mid = (start + end) // 2
+    if arr[mid] == mid:
+        return mid
+    elif arr[mid] > mid:
+        return binary_search(arr, start, mid - 1)
+    else:
+        return binary_search(arr, mid + 1, end)
+
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+result = binary_search(arr, 0, N - 1)
+
+print(result)
